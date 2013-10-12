79700623a191eabae6fdf6357734ea32004fd6d9
louisfettet <louisfettet@gmail.com>
2013-10-12 19:54:04 -0400

    Trying to get geolocation working

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,27 @@
-var map;
+var map, lat, lon;
 
 $(document).ready(function(){
-	map = L.mapbox.map('map', 'etrabert.map-wb3ujm4j')
-                 .setView([38.897, -77.0], 12);
+
+	// check if user has geolocation
+	if (navigator.geolocation) {
+	    console.log("user has geolocation!");
+	    navigator.geolocation.getCurrentPosition(
+		function(position) {
+		    console.log("hi");
+		    lat = position.coords.latitude;
+		    lon = position.coords.longitude;
+		    map = L.mapbox.map('map', 'etrabert.map-wb3ujm4j')
+			.setView([lat,lon],12);
+		    console.log("hurray!");
+		},
+		function(error){
+		    console.log("#oopsyall");
+		});
+	} else {
+	    console.log("user doesn't have geolocation :(");
+	    map = L.mapbox.map('map', 'etrabert.map-wb3ujm4j')
+		.setView([38.897, -77.0], 12);
+	}
 
     $(':checkbox').change(function() {
         console.log(this.value);
@@ -11,5 +30,6 @@ $(document).ready(function(){
 
     $('#learn-more').click(function(){
     	$('#about').slideToggle();
-    })
+    });
+
 });
