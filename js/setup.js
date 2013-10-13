01eadddb56da5a977d84a639af92c79757cef66d
Emily Trabert <emily.trabert@gmail.com>
2013-10-13 04:28:32 -0400

    Ready for test deploy.

--- js/setup.js
+++ js/setup.js
@@ -1,14 +1,15 @@
 var map, lat, lon, current, hungryJson, hungry, tiredJson, sickJson, dangerJson, markers;
 
 $.ajax({
-  url: 'json/test.json',
+  url: '69.175.214.21:4567',
   data: '',
   success: function(res){
   	// console.log(res);
-  	hungryJson = res.resp;
+  	hungryJson = res.response;
   	hungry = true;
 	for (var i = 0; i < hungryJson.length; i++) {
 		hungryJson[i].properties['marker-color'] = '#e5e500';
+		hungryJson[i].properties['marker-symbol'] = 'restaurant';
 		hungryJson[i].properties.description = 'Test';
 		hungryJson[i].properties.title = hungryJson[i].properties.name;
 	}
@@ -28,6 +29,7 @@ $.ajax({
   	tired = true;
 	for (var i = 0; i < tiredJson.length; i++) {
 		tiredJson[i].properties['marker-color'] = '#ffa500';
+		tiredJson[i].properties['marker-symbol'] = 'lodging';
 		tiredJson[i].properties.description = 'Test';
 		tiredJson[i].properties.title = tiredJson[i].properties.name;
 	}
@@ -87,8 +89,8 @@ $(document).ready(function(){
     
 
 	$(':checkbox').change(function() {
-		console.log(this.value);
-		console.log(this.checked);
+		// console.log(this.value);
+		// console.log(this.checked);
 		switch (this.value) {
 			case 'hungry': hungry = this.checked; break;
 			case 'tired': tired = this.checked; break;
@@ -101,33 +103,15 @@ $(document).ready(function(){
 		if (hungry) {
 			markers = markers.concat(hungryJson);
 		}
-		// if (tired) {
-		// 	markers = markers.concat(tiredJson);
-		// }
-		// if (sick) {
-		// 	if (sickJson == null) {
-		// 		$.get(url, function(data, status, jqxhr) {
-		// 			sickJson = data;
-		// 			for (var i = 0; i < sickJson.length; i++) {
-		// 				sickJson[i].properties['marker-color'] = 'red';
-		// 				sickJson[i].properties.description = 'Test';
-		// 			}
-		// 		});
-		// 	}
-		// 	markers = markers.concat(sickJson);
-		// }
-		// if (danger) {
-		// 	if (dangerJson == null) {
-		// 		$.get(url, function(data, status, jqxhr) {
-		// 			dangerJson = data;
-		// 			for (var i = 0; i < dangerJson.length; i++) {
-		// 				dangerJson[i].properties['marker-color'] = 'pink';
-		// 				dangerJson[i].properties.description = 'Test';
-		// 			}
-		// 		});
-		// 	}
-		// 	markers = markers.concat(dangerJson);
-		// }
+		if (tired) {
+			markers = markers.concat(tiredJson);
+		}
+		if (sick) {
+			markers = markers.concat(sickJson);
+		}
+		if (danger) {
+			markers = markers.concat(dangerJson);
+		}
 		map.markerLayer.setGeoJSON(markers);
 	});
 
@@ -168,12 +152,77 @@ function initialsetup() {
 
 	markers = current;
 	if (hungryJson) {
+		if (current != null) {
+			for (var i = 0; i < hungryJson.length; i++) {
+				hungryJson[i].properties.description += " <a href='https://maps.google.com/maps?q=from:+"+current[0].geometry.coordinates[1]+",+"+current[0].geometry.coordinates[0]+"+to:+"+hungryJson[i].geometry.coordinates[1]+",+"+hungryJson[i].geometry.coordinates[0]+"'>Directions</a>"
+			}
+		}
 		markers = markers.concat(hungryJson);
 	}
 	if (tiredJson) {
+		if (current != null) {
+			for (var i = 0; i < tiredJson.length; i++) {
+				tiredJson[i].properties.description += " <a href='https://maps.google.com/maps?q=from:+"+current[0].geometry.coordinates[1]+",+"+current[0].geometry.coordinates[0]+"+to:+"+tiredJson[i].geometry.coordinates[1]+",+"+tiredJson[i].geometry.coordinates[0]+"'>Directions</a>"
+			}
+		}
 		markers = markers.concat(tiredJson);
 	}
 	console.log(markers);
 
 	map.markerLayer.setGeoJSON(markers);
+
+	if (sickJson) {
+		if (current != null) {
+			for (var i = 0; i < sickJson.length; i++) {
+				sickJson[i].properties.description += " <a href='https://maps.google.com/maps?q=from:+"+current[0].geometry.coordinates[1]+",+"+current[0].geometry.coordinates[0]+"+to:+"+sickJson[i].geometry.coordinates[1]+",+"+sickJson[i].geometry.coordinates[0]+"'>Directions</a>"
+			}
+		}
+	}
+	if (dangerJson) {
+		if (current != null) {
+			for (var i = 0; i < dangerJson.length; i++) {
+				dangerJson[i].properties.description += " <a href='https://maps.google.com/maps?q=from:+"+current[0].geometry.coordinates[1]+",+"+current[0].geometry.coordinates[0]+"+to:+"+dangerJson[i].geometry.coordinates[1]+",+"+dangerJson[i].geometry.coordinates[0]+"'>Directions</a>"
+			}
+		}
+	}
 }
+
+$.ajax({
+  url: 'json/test.json',
+  data: '',
+  success: function(res){
+  	// console.log(res);
+  	sickJson = res.resp;
+  	sick = false;
+	for (var i = 0; i < sickJson.length; i++) {
+		sickJson[i].properties['marker-color'] = '#ce0000';
+		sickJson[i].properties['marker-symbol'] = 'hospital';
+		sickJson[i].properties.description = 'Test';
+		sickJson[i].properties.title = sickJson[i].properties.name;
+	}
+  },
+  error: function(jqXHR, textStatus, errorThrown){
+  	console.log("Sick get failed", textStatus);
+  },
+  dataType: 'json'
+});
+
+$.ajax({
+  url: 'json/test.json',
+  data: '',
+  success: function(res){
+  	// console.log(res);
+  	dangerJson = res.resp;
+  	danger = false;
+	for (var i = 0; i < dangerJson.length; i++) {
+		dangerJson[i].properties['marker-color'] = '#990099';
+		dangerJson[i].properties['marker-symbol'] = 'triangle';
+		dangerJson[i].properties.description = 'Test';
+		dangerJson[i].properties.title = dangerJson[i].properties.name;
+	}
+  },
+  error: function(jqXHR, textStatus, errorThrown){
+  	console.log("Danger get failed", textStatus);
+  },
+  dataType: 'json'
+});
