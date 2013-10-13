4f460b8d0e7769cb56591c3634ee2f13d4634931
Emily Trabert <emily.trabert@gmail.com>
2013-10-12 20:58:28 -0400

    Hunger toggles one dummy marker. Current location marker has a label.

--- js/setup.js
+++ js/setup.js
@@ -1,15 +1,15 @@
 var map, lat, lon, current, hungryJson, hungry, tiredJson, sickJson, dangerJson, markers;
 
-// $.get(url, success(data, status, jqxhr) {
-// 	hungryJson = data;
-// 	hungry = true;
-// 	for (var i = 0; i < hungryJson.length; i++) {
-// 		hungryJson[i].properties['marker-color'] = 'yellow';
-// 		hungryJson[i].properties.description = 'Test';
-// 	}
-// }, "json");
+$.get('json/json.json', function(data, status, jqxhr) {
+	hungryJson = data;
+	hungry = true;
+	for (var i = 0; i < hungryJson.length; i++) {
+		hungryJson[i].properties['marker-color'] = 'yellow';
+		hungryJson[i].properties.description = 'Test';
+	}
+}, "json");
 
-// $.get(url, success(data, status, jqxhr) {
+// $.get(url, function(data, status, jqxhr) {
 // 	tiredJson = data;
 // 	json = true;
 // 	for (var i = 0; i < tiredJson.length; i++) {
@@ -31,7 +31,8 @@ current = [{
 	},
 	properties: {
 		title: 'You Are Here',
-		'marker-color': '#555'
+		'marker-color': '#555',
+		'marker-symbol': 'star-stroked'
 	}
 }];
 
@@ -42,7 +43,6 @@ $(document).ready(function(){
 	    console.log("User has geolocation.");
 	    navigator.geolocation.getCurrentPosition(
 		function(position) {
-		    console.log("hi");
 		    lat = position.coords.latitude;
 		    lon = position.coords.longitude;
 		    map = L.mapbox.map('map', 'etrabert.map-wb3ujm4j')
@@ -77,16 +77,16 @@ $(document).ready(function(){
 			default: console.log("Invalid checkbox value?");
 		}
 
-		// markers = [];
-		// if (hungry) {
-		// 	markers.append(hungryJson);
-		// }
+		markers = current;
+		if (hungry) {
+			markers = markers.concat(hungryJson);
+		}
 		// if (tired) {
-		// 	markers.append(tiredJson);
+		// 	markers = markers.concat(tiredJson);
 		// }
 		// if (sick) {
 		// 	if (sickJson == null) {
-		// 		$.get(url, success(data, status, jqxhr) {
+		// 		$.get(url, function(data, status, jqxhr) {
 		// 			sickJson = data;
 		// 			for (var i = 0; i < sickJson.length; i++) {
 		// 				sickJson[i].properties['marker-color'] = 'red';
@@ -94,11 +94,11 @@ $(document).ready(function(){
 		// 			}
 		// 		});
 		// 	}
-		// 	markers.append(sickJson);
+		// 	markers = markers.concat(sickJson);
 		// }
 		// if (danger) {
 		// 	if (dangerJson == null) {
-		// 		$.get(url, success(data, status, jqxhr) {
+		// 		$.get(url, function(data, status, jqxhr) {
 		// 			dangerJson = data;
 		// 			for (var i = 0; i < dangerJson.length; i++) {
 		// 				dangerJson[i].properties['marker-color'] = 'pink';
@@ -106,9 +106,9 @@ $(document).ready(function(){
 		// 			}
 		// 		});
 		// 	}
-		// 	markers.append(dangerJson);
+		// 	markers = markers.concat(dangerJson);
 		// }
-		// map.markerLayer.setGeoJSON(markers);
+		map.markerLayer.setGeoJSON(markers);
 	});
 
 	$('#learn-more').click(function(){
@@ -118,39 +118,41 @@ $(document).ready(function(){
 });
 
 function initialsetup() {
-	markers = [{
-		type: 'Feature',
-		geometry: {
-			type: 'Point',
-			coordinates: [-77, 38.9]
-		},
-		properties: {
-			title: 'Marker One'
-		}
-	},
-	{
-		type: 'Feature',
-		geometry: {
-			type: 'Point',
-			coordinates: [-77.05, 38.95]
-		},
-		properties: {
-			title: 'Marker Two'
-		}
-	}];
+	// markers = [{
+	// 	type: 'Feature',
+	// 	geometry: {
+	// 		type: 'Point',
+	// 		coordinates: [-77, 38.9]
+	// 	},
+	// 	properties: {
+	// 		title: 'Marker One'
+	// 	}
+	// },
+	// {
+	// 	type: 'Feature',
+	// 	geometry: {
+	// 		type: 'Point',
+	// 		coordinates: [-77.05, 38.95]
+	// 	},
+	// 	properties: {
+	// 		title: 'Marker Two'
+	// 	}
+	// }];
 
-	for (var i = 0; i < markers.length; i++) {
-		markers[i].properties['marker-color'] = '#000000';
-		markers[i].properties.description = 'Test description';
-	}
+	// for (var i = 0; i < markers.length; i++) {
+	// 	markers[i].properties['marker-color'] = '#000000';
+	// 	markers[i].properties.description = 'Test description';
+	// }
 
 	
 
-	// markers = [];
-	// markers.append(hungryJson);
-	// markers.append(tiredJson);
+	markers = [];
+	markers = markers.concat(hungryJson);
+	// markers = markers.concat(tiredJson);
 	markers = markers.concat(current);
 	console.log(markers);
 
 	map.markerLayer.setGeoJSON(markers);
 }
+
+hungryJson = [{'type':'Feature', 'geometry':{'type':'Point', 'coordinates': [-77,38.9]},properties:{'eligibility':'7th - 9th grade','desc':'7 th grade through 9 th grade.  Strives to make dramatic improvements in the achievements of all students today in preparation for the world tomorrow.    ','address_1':'925 RHODE ISLAND AVENUE NW','area_served':'District of Columbia','address_2':'','keyword':'Schools-Public','email':'','web_url':'http//www.k12.dc.us','name':'DCG - DCPS - JHS - Shaw','business_hours':'800 am - 430 pm Monday - Friday','zip':'20001-4140','phone_1':'(202) 673-7203','marker-color': '#000'}}];
